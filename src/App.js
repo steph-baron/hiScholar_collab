635b5344fe4c29c33792fcb8448c4efb6d26a06f
bpaksoy <paksoybanu83@gmail.com>
2017-12-11 16:22:28 -0500

    Sticky footer added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import {Main} from  "react-materialize";
+import Main from  "./components/Main";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-          <main>
-            <div class="col s8">
-                 <div class="col s3">
-                 <img id="profileImg" src="/img/profile.jpg" alt="img"/>
-                 </div>
+      <div className="App Site">
+          <div className="Site-content">
+            <div className="App-header">
+              <Header />
+             </div>
+            <div className="main">
+              <Main/>
             </div>
-            <h1>Howdy</h1>
-            <h2>Hello world</h2>
-            <br/><br/><br/><br/>
-         </main>
+          </div>
         <Footer/>
       </div>
     );
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return(
+    <div>
+     <h1>Howdy</h1>
+     <h4>Hello</h4>
+       <img id="profileImg" src="/img/profile.jpg" alt="img"/>
+    </div>
+  );
+}
+
+export default Main;
